8a6e0feacb78b550298014605833412c0076c886
yashas <parteyashas@gmail.com>
2026-01-13 01:07:10 +0530

    working DVD screen saver on  i2c OLED

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,72 @@
-
-
-use esp_idf_svc::hal::delay::FreeRtos;
 use esp_idf_svc::hal::gpio::*;
-use esp_idf_svc::hal::peripherals::Peripherals;
+
+use esp_idf_svc::hal::{delay::FreeRtos, i2c::*, peripherals::Peripherals, units::FromValueType};
+use embedded_graphics::{
+    mono_font::{ascii::{FONT_6X10, FONT_10X20}, MonoTextStyle},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::Text,
+    primitives::{Circle, PrimitiveStyle},
+};
+
+use sh1106::{prelude::*, Builder};
 
 
 fn main() {
-    // It is necessary to call this function once. Otherwise, some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+
     esp_idf_svc::sys::link_patches();
 
     
     let peripherals = Peripherals::take().unwrap();
-    let mut led = PinDriver::output(peripherals.pins.gpio19).unwrap();
-
+    //i2c
+    let sda = peripherals.pins.gpio21;
+    let scl = peripherals.pins.gpio22;
+    
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-
-
+    let config = I2cConfig::new().baudrate(400.kHz().into());
+    let i2c = I2cDriver::new(peripherals.i2c0, sda, scl, &config).unwrap();
+    
+    let mut display: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
+    display.init().unwrap();
+    display.flush().unwrap();
+    
+    let text_style = MonoTextStyle::new(&FONT_10X20, BinaryColor::On);
+    
+    //aimation
+    let mut y  = 10;
+    let mut x = 20;
+    let mut dy  =2;
+    let mut dx = 2;
+    
+    
     log::info!("Hello, world!");
-
+    
     log::info!("this is gonna be crazy shit");
-
-
+    
+    let text = "DVD";
+    log::info!("{}",text);
+    
     loop {
-        led.set_high().unwrap();
-        FreeRtos::delay_ms(500);
-        led.set_low().unwrap();
-        FreeRtos::delay_ms(500);
+        display.clear();
+        x += dx;
+        y += dy;
+        if x < 0 || x > 100 {dx = -dx;}
+        if y < 10 || y > 60 {dy = -dy;}
+
+        Text::new(text, Point::new(10, y), text_style)
+        .draw(&mut display)
+        .unwrap();
+        display.flush().unwrap();
+        
+        Text::new("___", Point::new(10, y+2), text_style)
+        .draw(&mut display)
+        .unwrap();
+        display.flush().unwrap();
+        
+        FreeRtos::delay_ms(50);
+        
     }
+
 }
